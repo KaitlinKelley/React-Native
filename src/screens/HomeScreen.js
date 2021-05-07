561aa5a8d0c18154c4b3ae3ff4c91a517aadee2e
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-05-07 15:11:08 -0500

    react native buttons, touchable opacity, navigation, props object

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello!</Text>;
+//{navigation} instead of props -> example of object destructuring
+const HomeScreen = ({navigation}) => {
+  return (
+  <View>
+    <Text style={styles.text}>Hello!</Text>
+    <Button
+        title="Go to Components Demo"
+        // onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
+    />
+    <Button
+      title="Go to List Demo"
+      // onPress={() => props.navigation.navigate("List")}
+      onPress={() => navigation.navigate("List")}
+    />
+  </View>
+  );
 };
 
 const styles = StyleSheet.create({
